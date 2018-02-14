ff3aa7bbf85f5449cebd23aeb3193020fcbd81ad
Julian Rodriguez <felorodri@gmail.com>
2018-02-14 15:07:46 -0500

    log folder added. Main task modified to fetch entire db from any project from firebase

--- src/task.js
+++ src/task.js
@@ -1,7 +1,10 @@
-// Acquaring config file and dependencies:
+// Variables initialization
 var config = null;
-var firebase = require("firebase");
+var admin = null;
+var firebaseApps = [];
 
+// Acquaring config file and dependencies:
+admin = require("firebase-admin");
 try {
 	config = require('./config/databases.js');
 } catch (e) {
@@ -10,16 +13,39 @@ try {
 	process.exit(1);
 }
 
-firebase.initializeApp(config.projectsConfig[0].firebaseConfig);
+// Firebase apps initialization
+for (var i = 0; i < config.projectsConfig.length; i++) {
+	firebaseAppInit(config.projectsConfig[i]);
+}
 
+// Get database copy:
+if (!firebaseApps.length > 0) {
+	console.log("No databases foud to backup. Back up process aborted!");
+	process.exit(1);
+}else{
+	firebaseApps.forEach(dataFetcher);
+}
 
+function firebaseAppInit (item) {
+	try{
+		var serviceAccount = require(item.keyPath);
+		firebaseApps.push([item.projectName,
+			admin.initializeApp({
+			  credential: admin.credential.cert(serviceAccount),
+			  databaseURL: 'https://' + serviceAccount.project_id + '.firebaseio.com'
+			})]
+		)
+	} catch (e) {
+		console.log(e);
+		console.log('Project ' + item.projectName + ' couldnt be added to the queue');
+	}
+}
 
-
-
-
-
-
-
-// console.log(database);
-// console.log(config.projectsConfig[0].firebaseConfig);
-// process.exit(0);
+function dataFetcher (firebaseApp) {
+	var db = admin.database(firebaseApp[1]);
+	var ref = db.ref();
+	ref.once("value", function(snapshot) {
+	  console.log(snapshot.val());
+		process.exit(0);
+	});
+}
