fb31d241d7218ad4ec330d83a415ef65daf3b0da
Julian Rodriguez <felorodri@gmail.com>
2018-02-15 17:24:46 -0500

    Databases config file structure modified

--- src/config/databases.js
+++ src/config/databases.js
@@ -1,9 +1,15 @@
 //An array of project configurations
-const projects = [{
-  projectName: '<your-project-name>',
-  drivePath: '<your-gdrive-path-for-cloud-auto-backup>',
-  keyPath: '<firebase-auth-private-key-path>'
-}];
+const DEFAULT_KEY_FOLDER = './config/keys/';
+const DEFAULT_BACKUP_FOLDER = './backups/';
+const DEFAULT_MAX_BACKUP_FILES = 60;
+var projects = [
+	{
+	  projectName: '<your-project-name>',
+	  backupPath: DEFAULT_BACKUP_FOLDER,
+	  keyPath: DEFAULT_KEY_FOLDER + '<firebase-auth-private-key-path>',
+	  maxBackupNumber: DEFAULT_MAX_BACKUP_FILES
+	}
+];
 
 //Module exportation
 module.exports = {
