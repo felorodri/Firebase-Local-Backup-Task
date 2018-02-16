d38ef7d033b8a48524bb75b550b8ef83e51a6714
Julian Rodriguez <felorodri@gmail.com>
2018-02-16 12:04:12 -0500

    Backup process completed

--- src/task.js
+++ src/task.js
@@ -4,6 +4,7 @@ var firebaseApps = [];
 global.processedProjects = 0;
 const admin = require("firebase-admin");
 const fs = require('fs');
+const util = require('util');
 
 // Acquaring config file:
 try {
@@ -24,7 +25,14 @@ if (!firebaseApps.length > 0) {
 	console.log("No databases foud to backup. Back up process aborted!");
 }else{
 	firebaseApps.forEach(function(item, index){
-		dataFetcher(config.projectsConfig[index], item, index)
+		if (config.projectsConfig[index].maxBackupNumber > 0) {
+			dataFetcher(config.projectsConfig[index], item, index)
+		} else {
+			var logMessage = '\nNo backup placed for project ' + config.projectsConfig[index].projectName + '. Check the max buckup number in your project settings!';
+			console.log(logMessage);
+			processedProjects++;
+			logger(logMessage);
+		}
 	});
 }
 
@@ -49,42 +57,46 @@ function dataFetcher (projectConfig, item, index) {
 	var ref = db.ref();
 	ref.once('value')
     .then(function (snapshot) {
-    	const dbData = snapshot.val();
     	try {
+    		const dbData = snapshot.val();
     		var folderName = projectConfig.backupPath + projectConfig.projectName;
 	      if (!fs.existsSync(folderName)){
 	          fs.mkdirSync(folderName);
 	      }
-
-
 	      var existingFiles = fs.readdirSync(folderName);
-	      
-
-
-
-	      if (existingFiles.length > 0){
-	       	console.log(existingFiles);
-	      }
-	      
-
-
-
-
+				if (existingFiles.length >= projectConfig.maxBackupNumber) {
+					var numberToRemove = Math.abs(existingFiles.length - projectConfig.maxBackupNumber) + 1;
+					var backupsToRemove = [];
+					var backupsData = [];
+					var timestampsArray = [];
+		      for (var i = 0; i < existingFiles.length; i++) {
+			      var backupStats = fs.statSync(folderName + '/' + existingFiles[i]);
+						var mtime = new Date(util.inspect(backupStats.mtime));
+						backupsData.push({
+							fileName: existingFiles[i],
+							timestamp: mtime.getTime() 
+						});
+						timestampsArray.push(mtime.getTime());
+		      }
+		      timestampsArray.sort(function(a, b){return b-a});
+		      for (var k = 0; k < numberToRemove; k++) {
+		      	for (var j = 0; j < backupsData.length; j++) {
+		      		if (backupsData[j].timestamp == timestampsArray[k]) {
+		      			backupsToRemove.push(backupsData[j].fileName);
+		      			break;
+		      		}
+		      	}
+		      }
+		      for (var l = 0; l < backupsToRemove.length; l++) {
+		      	fs.unlinkSync(projectConfig.backupPath + projectConfig.projectName + '/' + backupsToRemove[l]);
+		      }
+				}	      
 	      // var fileName = projectConfig.projectName + '_' + new Date().getTime().toString() + '.json'; // Using timestamp
 	      var fileName = projectConfig.projectName + '_' + ((new Date().toString()).replace(/ /gi, '_').replace(/:/gi, '-')) + '.json'; // Using date string
-
-
-
-
-
-	      try {
-					fs.writeFileSync(folderName + '/' + fileName, JSON.stringify(dbData, null, 4));
-					console.log('New backup saved for project ' + projectConfig.projectName + '!');
-					processedProjects++;
-	      	processedProjectsChecker('');
-	      } catch(e) {
-	      	throw e;
-	      }
+				fs.writeFileSync(folderName + '/' + fileName, JSON.stringify(dbData, null, 4));
+				console.log('New backup saved for project ' + projectConfig.projectName + '!');
+				processedProjects++;
+				processedProjectsChecker('');
     	} catch (e) {
     		var logMessage = e.toString() + '\nProject ' + projectConfig.projectName + ' backup couldnt be written on disk.';
     		processedProjects++;
